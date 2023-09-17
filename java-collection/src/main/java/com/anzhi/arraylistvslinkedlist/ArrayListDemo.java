e2b0f827176d0c02c8b9bcef4a11c5e413493131
azh <2550364626@qq.com>
2023-09-17 18:20:57 +0800

    LinkedList 与 ArrayList-插入效率

--- java-collection/src/main/java/com/anzhi/arraylistvslinkedlist/ArrayListDemo.java
+++ java-collection/src/main/java/com/anzhi/arraylistvslinkedlist/ArrayListDemo.java
@@ -5,16 +5,42 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ArrayListDemo {
+
+    // 从头部添加数组元素测试标志
+    private static final boolean ADD_HEAD_FLAG = false;
+    // 从中间位置添加元素
+    private static final boolean ADD_MIDDLE_FLAG = true;
+
+    // API 查看 demo 标志
+    private static final boolean API_INDEX_FLAG = false;
+
+    // 插入元素
+    private static final int ADD_NUM_SIZE = 10000;
+
     public static void main(String[] args) {
-        List<String> arrayList = new ArrayList<>();
-        for (int i=0; i<10; i++) {
-            arrayList.add("第" + i + "元素");
-        }
-        for (int j=0; j<arrayList.size(); j++) {
-            String nodeStr = arrayList.get(j);
-            if (nodeStr.equals("第6个元素")) {
-                break;
+        ArrayList<String> arrayList = new ArrayList<>();
+        if (API_INDEX_FLAG) {
+            for (int i = 0; i < 10; i++) {
+                arrayList.add("第" + i + "元素");
             }
+            for (int j = 0; j < arrayList.size(); j++) {
+                String nodeStr = arrayList.get(j);
+                if (nodeStr.equals("第6个元素")) {
+                    break;
+                }
+            }
+        } else if (ADD_HEAD_FLAG) {
+            long timeEnd = System.currentTimeMillis();
+            for (int i = 0; i < ADD_NUM_SIZE; i++) {
+                arrayList.add(0, "第" + i +"个元素");
+            }
+            System.out.println("ArrayList 从头添加" + ADD_NUM_SIZE + "个元素花费时间为：" + (System.currentTimeMillis() - timeEnd));
+        } else if (ADD_MIDDLE_FLAG) {
+            long timeEnd = System.currentTimeMillis();
+            for (int i = 0; i < ADD_NUM_SIZE; i++) {
+                arrayList.add(arrayList.size()/2, "第" + i +"个元素");
+            }
+            System.out.println("ArrayList 从中间位置添加" + ADD_NUM_SIZE + "个元素花费时间为：" + (System.currentTimeMillis() - timeEnd));
         }
     }
 }
--- java-collection/src/main/java/com/anzhi/arraylistvslinkedlist/LinkedListDemo.java
+++ java-collection/src/main/java/com/anzhi/arraylistvslinkedlist/LinkedListDemo.java
@@ -5,24 +5,56 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListDemo {
-    public static void main(String[] args) {
-        List<String> linkedList = new LinkedList<>();
-        for (int i=0; i<10; i++) {
-            linkedList.add("第" + i + "个元素");
-        }
-        for (int j=0; j<linkedList.size(); j++) {
-            String nodeStr = linkedList.get(j);
-            if (nodeStr.equals("第6个元素")) {
-                break;
-            }
-        }
 
-        Iterator<String> iterator = linkedList.iterator();
-        while(iterator.hasNext()) {
-            String nodeStr = iterator.next();
-            if (nodeStr.equals("第6个元素")) {
-                break;
+    // 从头部添加数组元素测试标志
+    private static final boolean ADD_HEAD_FLAG = false;
+
+    // 从中间位置添加元素
+    private static final boolean ADD_MIDDLE_FLAG = true;
+
+    // API 查看 demo 标志
+    private static final boolean API_INDEX_FLAG = false;
+
+    // 插入元素
+    private static final int ADD_NUM_SIZE = 10000;
+
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<String>();
+        if (API_INDEX_FLAG) {
+            for (int i = 0; i < 10; i++) {
+                linkedList.add("第" + i + "个元素");
             }
+            for (int j = 0; j < linkedList.size(); j++) {
+                String nodeStr = linkedList.get(j);
+                if (nodeStr.equals("第6个元素")) {
+                    break;
+                }
+            }
+
+            linkedList.add(6, "插入一个元素在下标为6的数据之前");
+
+            Iterator<String> iterator = linkedList.iterator();
+            while (iterator.hasNext()) {
+                String nodeStr = iterator.next();
+                if (nodeStr.equals("第6个元素")) {
+                    break;
+                }
+            }
+        } else if (ADD_HEAD_FLAG) {
+            long timeEnd = System.currentTimeMillis();
+            for (int i = 0; i < ADD_NUM_SIZE; i++) {
+                linkedList.addFirst("第" + i + "个元素");
+            }
+            System.out.println("LinkedList 从头添加" + ADD_NUM_SIZE + "个元素花费时间为：" + (System.currentTimeMillis() - timeEnd));
+        } else if (ADD_MIDDLE_FLAG) {
+            long timeEnd = System.currentTimeMillis();
+            for (int i = 0; i < ADD_NUM_SIZE; i++) {
+                linkedList.add(linkedList.size()/2, "第" + i + "个元素");
+            }
+            System.out.println("LinkedList 从中间添加" + ADD_NUM_SIZE + "个元素花费时间为：" + (System.currentTimeMillis() - timeEnd));
         }
     }
+
 }
+
+
