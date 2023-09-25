a1b1f39b8d841753dddd788cb0106ececbc37c08
azh <2550364626@qq.com>
2023-09-25 10:57:30 +0800

    LinkedList 与 ArrayList-删除效率

--- java-collection/src/main/java/com/anzhi/hashmapvshashtable/HashMapDemo.java
+++ java-collection/src/main/java/com/anzhi/hashmapvshashtable/HashMapDemo.java
@@ -5,7 +5,15 @@ import java.util.Map;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        Map<String,String> map = new HashMap<>();
-        map.put(null, null);
+        Map<Object,String> map = new HashMap<>();
+        for (int i=0; i<2; i++) {
+            map.put(i, "存放第" + i + "个元素");
+        }
+        for (int binCount = 0;;++binCount) {
+            if (binCount >= 8 - 1) {
+                System.out.printf("111111111111= %d", binCount);
+                break;
+            }
+        }
     }
 }
